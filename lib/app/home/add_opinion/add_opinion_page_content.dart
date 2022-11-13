4de5d8837b916e157b116d38bc230b353212018b
Łukasz Piechowski <ukaasz0@gmail.com>
2022-11-13 20:57:30 +0100

    Order restaurants with rating

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var burgerName = '';
+  var rating = 3.0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę restauracji',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę burgera',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                burgerName = newValue;
+              });
+            },
+          ),
+          Slider(
+            onChanged: (newValue) {
+              setState(() {
+                rating = newValue;
+              });
+            },
+            value: rating,
+            min: 1.0,
+            max: 6.0,
+            divisions: 10,
+            label: rating.toString(),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'burger': burgerName,
+                'rating': rating,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
