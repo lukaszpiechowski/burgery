06f0e54e0021f4248a196625fe2c16032503f4db
Łukasz Piechowski <ukaasz0@gmail.com>
2022-12-12 19:23:18 +0100

    Move home and login into features folder

--- lib/app/features/home/restaurants/restaurants_page_content.dart
+++ lib/app/features/home/restaurants/restaurants_page_content.dart
@@ -1,4 +1,4 @@
-import 'package:burgery/app/home/restaurants/cubit/restaurants_cubit.dart';
+import 'package:burgery/app/features/home/restaurants/cubit/restaurants_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:burgery/app/home/home_page.dart';
-import 'package:burgery/app/login/login_page.dart';
+import 'package:burgery/app/features/home/home_page.dart';
+import 'package:burgery/app/features/login/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
