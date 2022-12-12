0a306b7e24066108de8b3d76075a52e5c04fdb01
Łukasz Piechowski <ukaasz0@gmail.com>
2022-12-12 13:43:13 +0100

    Change const

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -84,7 +84,7 @@ class _LoginPageState extends State<LoginPage> {
                       isCreatingAccount = true;
                     });
                   },
-                  child: Text('Utwórz konto'),
+                  child: const Text('Utwórz konto'),
                 ),
               ],
               if (isCreatingAccount == true) ...[
@@ -94,7 +94,7 @@ class _LoginPageState extends State<LoginPage> {
                       isCreatingAccount = false;
                     });
                   },
-                  child: Text('Masz już konto?'),
+                  child: const Text('Masz już konto?'),
                 ),
               ],
             ],
